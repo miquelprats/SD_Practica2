9922c769609ade298564a335bfd31a7e571715c1
miquelprats <58902985+miquelprats@users.noreply.github.com>
2020-05-22 20:27:11 +0200

    All functionality done.

--- main.py
+++ main.py
@@ -1,79 +1,75 @@
 import pywren_ibm_cloud as pywren
-import time,datetime
-#from datetime import datetime
-import pickle
-
+import time,datetime, pickle, pytz
 
 bucket_name='practica-sd-mp'
-#someone_in=False
-N_SLAVES = 5
-if N_SLAVES <=0: N_SLAVES=1
+
+N_SLAVES = 10
+if N_SLAVES <=0: 
+    N_SLAVES=1
+elif N_SLAVES>100:
+    N_SLAVES=100
 
 def master(id, x, ibm_cos):
     write_permission_list = []
-
     done=0
     ibm_cos.put_object(Bucket=bucket_name,Key='result.txt')
-    time.sleep(1)
-    ultimaActualitzacio=ibm_cos.get_object(Bucket=bucket_name,Key='result.txt')['LastModified']
+    time.sleep(4)
+
     first=True
+    ultimaActualitzacio=pytz.utc.localize(datetime.datetime.now())
     while done<N_SLAVES:
         ara=ibm_cos.get_object(Bucket=bucket_name,Key='result.txt')['LastModified']
         if(ultimaActualitzacio<ara or first):
-            first=False
-            list_files=ibm_cos.list_objects(Bucket=bucket_name, Marker='p_write_')['Contents']
-            slaves_bucket=[]
-            for fitxer in list_files:
-                slaves_bucket.append({"NomFitxer":fitxer['Key'], "DataCreacio":fitxer['LastModified']})
-            #ordenem per la data de modificacio
-            slaves_bucket=sorted(slaves_bucket,key=lambda k:k['DataCreacio'])
-            #fiquem el write_{id}, per fer aixo borrem el p_ del nom
-            ibm_cos.put_object(Bucket=bucket_name,Key=slaves_bucket[0]['NomFitxer'][2:])
-            ibm_cos.delete_object(Bucket=bucket_name,Key='ninguExecutant')
-            ibm_cos.delete_object(Bucket=bucket_name,Key=slaves_bucket[0]['NomFitxer'])
-            write_permission_list.append(id)
-            done+=1
-      
+            aux=done
+            #Since it's in, it can't get out without giving permission to one slave
+            while aux==done:
+                if first:
+                    first=False
+                list_files=ibm_cos.list_objects(Bucket=bucket_name, Marker='p_')['Contents']
+                slaves_bucket=[]
+                ultimaActualitzacio=ara
+                for fitxer in list_files:
+                    if(fitxer['Key'][0:2] == 'p_'):
+                        slaves_bucket.append({"NomFitxer":fitxer['Key'], "DataCreacio":fitxer['LastModified']})
+                
+                if(len(slaves_bucket)!=0):
+                    #order for time of creation
+                    slaves_bucket=sorted(slaves_bucket,key=lambda k:k['DataCreacio'])
+                    #Deleting p_ so it remains write_id
+                    ibm_cos.put_object(Bucket=bucket_name,Key=slaves_bucket[0]['NomFitxer'][2:])
+                    write_permission_list.append(slaves_bucket[0]['NomFitxer'][8:])
+                    ibm_cos.delete_object(Bucket=bucket_name,Key=slaves_bucket[0]['NomFitxer'])
+                    done+=1
         time.sleep(2)
     return write_permission_list
- # 1. monitor COS bucket each X seconds
- # 2. List all "p_write_{id}" files
- # 3. Order objects by time of creation
- # 4. Pop first object of the list "p_write_{id}"
- # 5. Write empty "write_{id}" object into COS
- # 6. Delete from COS "p_write_{id}", save {id} in write_permission_list
- # 7. Monitor "result.json" object each X seconds until it is updated
- # 8. Delete from COS “write_{id}”
- # 8. Back to step 1 until no "p_write_{id}" objects in the bucket
+
 
 
 
 
 def slave(id, x, ibm_cos):
     ibm_cos.put_object(Bucket=bucket_name,Key=f'p_write_{id}')
-    while True:
+    fet=True
+    while fet:
         try:
+            #check if you have permission (write_{id} exists)
             ibm_cos.get_object(Bucket=bucket_name, Key=f'write_{id}')
-            #ibm_cos.put_object(Bucket=bucket_name,Key=f'{id}')
-            #ibm_cos.delete_object(bucket_name, Key=f'write_{id}')
+            ibm_cos.delete_object(Bucket=bucket_name, Key=f'write_{id}')
+            fet=False
+            #try for checking if it's empty
             try:
-                content=pickle.loads(ibm_cos.get_object(Bucket=bucket_name, Key='result.txt')['Body'].read())
+                result=ibm_cos.get_object(Bucket=bucket_name, Key='result.txt')['Body'].read()
+                content=pickle.loads(result)
                 content.append(f'{id}')
             except Exception:
-                content=f'{id}'
-            ibm_cos.put_object(Bucket=bucket_name, Key='result.txt', Body= pickle.dumps(content))
-            return
+                content=[]
+                content.append(f'{id}')
+            
+            ibm_cos.put_object(Bucket=bucket_name, Key='result.txt', Body= pickle.dumps(content))         
         except Exception:
             pass
-        time.sleep(5)
+        time.sleep(4)
 
-    
-    #cos.put_object(bucket_name,'buit','No hi ha ningú al result.txt')
- # 1. Write empty "p_write_{id}" object into COS
- # 2. Monitor COS bucket each X seconds until it finds a file called "write_{id}"
- # 3. If write_{id} is in COS: get result.txt, append {id}, and put back to COS result.txt
- # 4. Finish
- # No need to return anything
 
 
 if __name__ == '__main__':
@@ -81,9 +77,16 @@ if __name__ == '__main__':
     pw.call_async(master, 0)
     pw.map(slave, range(N_SLAVES))
     write_permission_list = pw.get_result()
-    print(write_permission_list)
+    
+    print(f'The list from the master: {write_permission_list}')
 
-
-    #print(pickle.loads(cos.get_object(bucket_name,'result.txt')))
- # Get result.txt
- # check if content of result.txt == write_permission_list
+    ibm_cos = pw.internal_storage.get_client()
+    result = ibm_cos.get_object(Bucket=bucket_name, Key='result.txt')['Body'].read()
+    print(f'The result.txt file: {pickle.loads(result)}')
+    ibm_cos.put_object(Bucket=bucket_name, Key='p_write_1000')
+    list_files=ibm_cos.list_objects(Bucket=bucket_name)['Contents']
+    #Deleting all remaining files (including some that should be deleted previously)
+    created_files=['p_write_','write_', 'result.txt']   
+    for elem in list_files:
+        if(elem['Key'] in created_files):
+            ibm_cos.delete_object(Bucket=bucket_name, Key=elem['Key'])
